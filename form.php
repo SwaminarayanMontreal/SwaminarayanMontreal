5848f67a3dd3e757bf90fbd1cb2fcd0a3df69de3
SwaminarayanMontreal <87553612+SwaminarayanMontreal@users.noreply.github.com>
2021-07-18 16:19:30 -0400

    Add files via upload

--- form.php
+++ form.php
@@ -1,18 +1,18 @@
 <?php
 
-$firstname = $_POST['firstname'];
-$lastname = $_POST['lastname'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+// $firstname = $_POST['firstname'];
+// $lastname = $_POST['lastname'];
+// $email = $_POST['email'];
+// $message = $_POST['message'];
 
-$to = "swaminarayanmontreal@gmail.com";
-$subject = "MSSMV info Required";
+// $to = "swaminarayanmontreal@gmail.com";
+// $subject = "MSSMV info Required";
 
-$body = "This an email from MSSMV website from $firstname \n 
-Message $message \n  Sender contact info: $email";
+// $body = "This an email from MSSMV website from $firstname \n 
+// Message $message \n  Sender contact info: $email";
 
-mail($to,$subject,$message);
+// mail($to,$subject,$message);
 
-echo "Message Sent Successfully.";
+// echo "Message Sent Successfully.";
 
-?>
+?> 
